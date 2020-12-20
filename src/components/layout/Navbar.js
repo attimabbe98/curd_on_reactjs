09819e11139e3c6b258336d11d9d09e0bf1dad54
attimabbe98 <hosamaniattimabbe96@gmail.com>
2020-12-21 01:34:50 +0530

    first commit

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container">
         <Link className="navbar-brand" href="/">
-          Recat User
+          React user
         </Link>
         <button
           className="navbar-toggler"
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,19 +6,16 @@ const About = () => {
       <div className="py-4">
         <h1>About Page</h1>
         <p className="lead">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
-          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
-          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
-          laborum dolorum libero maiores!
+         hope for best team!to explore something
         </p>
 
         <p className="lead">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
-          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
-          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
-          laborum dolorum libero maiores!
+          this is learning time to explore new things
+</p>
+
+           <p>     -attimabbe
         </p>
-        <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt nam accusantium? Soluta cupiditate, accusamus commodi praesentium laborum dolorum libero maiores!</p>
+        <p className="lead">  </p>
       </div>
     </div>
   );
